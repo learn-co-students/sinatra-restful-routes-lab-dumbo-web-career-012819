84cb06fc5d4ff12cd4f11dbaebd79dbd6c496349
mazenswar <mazenswar@protonmail.com>
2019-02-20 15:51:32 -0500

    restful routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,39 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+get('/recipes') { 
+  @recipes = Recipe.all
+  erb :index
+ }
+
+get('/recipes/new') { erb :new }
+
+post('/recipes') {
+  @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+  redirect to "recipes/#{@recipe.id}"
+}
+
+get('/recipes/:id') {
+  @recipe = Recipe.find(params[:id])
+  erb :show
+}
+
+delete('/recipes/:id') {
+  @recipe = Recipe.find(params[:id])
+  Recipe.delete(params[:id])
+  redirect to '/recipes'
+}
+
+get('/recipes/:id/edit') {
+  @recipe = Recipe.find(params[:id])
+  erb :edit
+}
+
+patch('/recipes/:id') {
+  @recipe = Recipe.find(params[:id])
+  @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+  redirect to "/recipes/#{@recipe.id}"
+}
+
+
 end
