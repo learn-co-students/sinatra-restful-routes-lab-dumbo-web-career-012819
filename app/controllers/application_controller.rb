eeaaf541585d6f2f53004f850e6f8b2a61985c4b
github username <github email address>
2019-02-20 20:39:17 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,51 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+      @recipes = Recipe.all
+     #binding.pry
+      erb :index
+  end
 
+
+    get  '/recipes/new' do
+        erb :new
+    end
+
+    post '/recipes' do
+        @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+        # binding.pry
+
+        redirect to "/recipes/#{@recipe.id}"
+    end
+
+   get  '/recipes/:id' do
+       @recipes = Recipe.find_by_id(params[:id])
+       erb :show
+   end
+
+   delete '/recipes/:id' do
+       @recipes = Recipe.find_by_id(params[:id])
+       @recipes.delete
+       redirect to '/recipes'
+   end
+
+   get '/recipes/:id/edit' do
+       @recipe = Recipe.find_by_id(params[:id])
+       erb :edit
+   end
+
+   patch '/recipes/:id' do
+       #binding.pry
+       @recipe = Recipe.find_by_id(params[:id])
+       @recipe.name = params[:name]
+       @recipe.ingredients = params[:ingredients]
+       @recipe.cook_time = params[:cook_time]
+       @recipe.save
+       redirect to "/recipes/#{@recipe.id}"
+   end
 end
